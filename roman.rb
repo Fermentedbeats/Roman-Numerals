6e1693272fb2d4157f3f3bb0e3c2e7e1926e2b0d
CJ Johnson <fermentedbeats@gmail.com>
2014-08-05 11:51:04 -0700

    init

--- roman.rb
+++ roman.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
 
 get("/") do
-
-  "<form><input name="int"/><button>submit</button></form>"+
+  arabic_string = params.fetch("roman")
+  arabic_number = arabic_string.to_i
+  roman=romnum(arabic_number)
+  "<form><input name='roman'/><button>submit</button></form>"+
   "<h1>#{roman}</h1>"
+
 end
 
 def romnum(int)
